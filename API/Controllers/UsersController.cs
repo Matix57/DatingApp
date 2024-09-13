2bf7419888fb268a4eb348d017503eb2ce93a7e7
Mateusz Markiewicz <mateusz.markiewicz01@gmail.com>
2024-09-13 22:16:03 +0200

    End of section 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,7 +9,6 @@ namespace API;
 [Route("api/[controller]")] // /api/users
 public class UsersController(DataContext context) : ControllerBase
 {
-
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -18,12 +17,12 @@ public class UsersController(DataContext context) : ControllerBase
         return users;
     }
 
-    [HttpGet("{id:int}")] // /api/users/
-    public async Task<ActionResult<AppUser>> GetUsers(int id)
+    [HttpGet("{id:int}")]  // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
     {
         var user = await context.Users.FindAsync(id);
 
-        if(user == null) return NotFound();
+        if (user == null) return NotFound();
 
         return user;
     }
