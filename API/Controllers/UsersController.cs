2e2b6b9f07b354717a75e31d6eed9e22ef26bd53
Mateusz Markiewicz <mateusz.markiewicz01@gmail.com>
2024-10-27 22:11:44 +0100

    End of section 12

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -60,7 +60,10 @@ public class UsersController(IUserRepository userRepository, IMapper mapper,
         {
             Url = result.SecureUrl.AbsoluteUri,
             PublicId = result.PublicId
+
         };
+
+        if (user.Photos.Count == 0) photo.IsMain = true;
 
         user.Photos.Add(photo);
 
